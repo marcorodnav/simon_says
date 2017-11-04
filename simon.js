18a1347cc2f9f74203a8df3260727b42deef0d05
marcorodnav <marcorodnav@gmail.com>
2017-11-04 16:44:04 -0600

    added on/off control added on/off action

--- simon.js
+++ simon.js
@@ -1,15 +1,10 @@
 // Selected actions
 (function () {
-  const resetToggleBtn = document.querySelector('#resetToggle')
+  
   const strictToggleBtn = document.querySelector('#strictToggle')
+  const onOffToggleBtn = document.querySelector('div.onOff')
+  const onOffIndicator = document.querySelector('#indicator')
 
-  resetToggleBtn.onclick = () => {
-    if (resetToggleBtn.classList.contains('redSelected')) {
-      resetToggleBtn.classList.remove('redSelected')
-    } else {
-      resetToggleBtn.classList.add('redSelected')
-    }
-  };
   strictToggleBtn.onclick = () => {
     if (strictToggleBtn.classList.contains('yellowSelected')) {
       strictToggleBtn.classList.remove('yellowSelected')
@@ -17,4 +12,25 @@
       strictToggleBtn.classList.add('yellowSelected')
     }
   };
+
+  onOffToggleBtn.onclick = () => {
+    if (onOffToggleBtn.classList.contains('on')) {
+      onOffToggleBtn.classList.remove('on')
+      onOffToggleBtn.classList.add('off')
+      onOffIndicator.textContent = 'OFF'
+      document.querySelector('#controlsPanel').style.background = "black"
+      document.querySelector('#counter').style.color = "black";
+      strictToggleBtn.style.background = "black";
+      document.querySelector('#resetToggle').style.background = "black"
+    } else {
+      onOffToggleBtn.classList.remove('off')
+      onOffToggleBtn.classList.add('on')
+      onOffIndicator.textContent = 'ON'
+      document.querySelector('#controlsPanel').style.background = "white"
+      document.querySelector('#counter').style.color = "red";
+      strictToggleBtn.style.background = "radial-gradient(#ff0,#000)"
+      document.querySelector('#resetToggle').style.background = "radial-gradient(#f00,#000)"      
+    }
+  }
+
 })();
