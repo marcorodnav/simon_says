6897302fb6a43f5fc7278d066112e92047816b24
marcorodnav <marcorodnav@gmail.com>
2018-01-05 22:07:18 -0600

    modifiy the aiPlay function

--- simon.js
+++ simon.js
@@ -1,10 +1,52 @@
-// Selected actions
-(function () {
-  
+ 
+  // Controls
   const strictToggleBtn = document.querySelector('#strictToggle')
   const onOffToggleBtn = document.querySelector('div.onOff')
   const onOffIndicator = document.querySelector('#indicator')
+  const resetToggleBtn = document.querySelector('#resetToggle')
+  const simBtn = document.querySelector('#sim')
 
+  // Color panels
+  const greenPanel = document.querySelector('#greenPanel')
+  const redPanel = document.querySelector('#redPanel')
+  const yellowPanel = document.querySelector('#yellowPanel')
+  const bluePanel = document.querySelector('#bluePanel')
+
+  const panels = {
+    "0": {
+      index: 0,
+      panel: greenPanel,
+      sound: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3')
+    },
+    "1": {
+      index: 1,
+      panel: redPanel,
+      sound: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3')
+    },
+    "2": {
+      index: 2,
+      panel: yellowPanel,
+      sound: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3')
+    },
+    "3": {
+      index: 3,
+      panel: bluePanel,
+      sound: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
+    }
+  }
+
+  // Board sounds
+  const sounds = {
+    "0": new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3'),
+    "1": new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'),
+    "2": new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'),
+    "3": new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'),
+    playSound: (sound) => {
+      sounds[sound].play()
+    }
+  }
+
+  // Strict mode
   strictToggleBtn.onclick = () => {
     if (strictToggleBtn.classList.contains('yellowSelected')) {
       strictToggleBtn.classList.remove('yellowSelected')
@@ -13,6 +55,7 @@
     }
   };
 
+  // On/Off
   onOffToggleBtn.onclick = () => {
     if (onOffToggleBtn.classList.contains('on')) {
       onOffToggleBtn.classList.remove('on')
@@ -22,6 +65,7 @@
       document.querySelector('#counter').style.color = "black";
       strictToggleBtn.style.background = "black";
       document.querySelector('#resetToggle').style.background = "black"
+      strictToggleBtn.classList.remove('yellowSelected')
     } else {
       onOffToggleBtn.classList.remove('off')
       onOffToggleBtn.classList.add('on')
@@ -32,4 +76,77 @@
       document.querySelector('#resetToggle').style.background = "radial-gradient(#f00,#000)"      
     }
   }
-})();
+
+  // Board sounds
+  greenPanel.onclick = () => {
+    sounds.playSound('0')
+  }
+  redPanel.onclick = () => {
+    sounds.playSound('1')
+  }
+  yellowPanel.onclick = () => {
+    sounds.playSound('2')
+  }
+  bluePanel.onclick = () => {
+    sounds.playSound('3')
+  }
+
+  // simulate greenPanel Active
+  
+  const first = () => {
+    setTimeout((function() {
+      simGreenActive()
+      setTimeout(restorePanels, 450)
+    }), 450);
+  }
+  const simGreenActive = () => {
+    greenPanel.style['background-color'] = '#079607'
+    sounds.playSound('1')
+  }
+
+  const activatePanel = (step) => {
+    setTimeout(() => {
+      step.panel.classList.add('light');
+      setTimeout(() => {
+        step.panel.classList.remove('light');  
+      }, 250, step);
+    }, 250, step);
+  }
+
+  const srcSounds = [
+    'https://s3.amazonaws.com/freecodecamp/simonSound1.mp3',
+    'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3',
+    'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3',
+    'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3',
+  ]
+  
+  const aiPlay = (simon) => {
+    let soundIndex;
+    setTimeout(() => {
+      for(var i = 0; i < simon.length; i++) {
+        (function(i) {
+          setTimeout(() => {
+            // play sound
+            sounds[simon[i]].play()
+            // activate(tiemout 150) / deactivate(timeout 150)
+            activatePanel(panels[simon[i]]);
+          }, i * 800);
+        })(i);
+      }
+    }, 1000);
+    // set player turn active
+  }
+
+  
+
+  simBtn.onclick = () => {
+    aiPlay(['1','0'])
+  }
+  
+  resetToggleBtn.onclick = () => {
+    restoreGreenPanel()
+  }
+  
+
+
+
